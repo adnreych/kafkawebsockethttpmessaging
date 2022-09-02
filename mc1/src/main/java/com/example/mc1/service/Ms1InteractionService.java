c1475fbb65128334236986d5a8adac1390ab816e
adnreych <adnreych@gmail.com>
2022-09-02 21:04:56 +0300

    send object from mc1

--- mc1/src/main/java/com/example/mc1/service/Ms1InteractionService.java
+++ mc1/src/main/java/com/example/mc1/service/Ms1InteractionService.java
@@ -3,6 +3,10 @@ package com.example.mc1.service;
 import com.example.common.model.CircularMessage;
 import com.example.common.model.ServiceName;
 import com.example.common.service.InteractionService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
@@ -22,24 +26,20 @@ public class Ms1InteractionService {
     }
 
     public void startInteraction() {
-        log.info("STAAARTED");
         CircularMessage circularMessage = new CircularMessage();
         interactionService.provideInteraction(ServiceName.MS1, circularMessage);
         try {
             final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(new URI("ws://localhost:8083/socketHandler"));
+            ObjectMapper mapper = JsonMapper.builder()
+                    .addModule(new JavaTimeModule())
+                    .build();
+            String json = mapper.writeValueAsString(circularMessage);
+            clientEndPoint.sendMessage(json);
 
-            clientEndPoint.addMessageHandler(new WebsocketClientEndpoint.MessageHandler() {
-                public void handleMessage(String message) {
-                    System.out.println(message);
-                }
-            });
-            clientEndPoint.sendMessage("MMMEEESSAGE}");
-            Thread.sleep(5000);
-
-        } catch (InterruptedException ex) {
-            System.err.println("InterruptedException exception: " + ex.getMessage());
         } catch (URISyntaxException ex) {
             System.err.println("URISyntaxException exception: " + ex.getMessage());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
         }
 
     }
